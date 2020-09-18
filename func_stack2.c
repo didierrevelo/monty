982af6613e3b5f479cec6d536125c8fbd18adfa8
didierrevelo <1829@holbertonschool.com>
2020-09-18 03:15:32 -0500

    valgrind

--- func_stack2.c
+++ func_stack2.c
@@ -7,7 +7,7 @@
  */
 void _add(stack_t **stack, unsigned int line_number)
 {
-	if ((*stack)->next == NULL)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
